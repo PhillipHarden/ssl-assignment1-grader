64a26919a3dab3bc78a9527ddde7078110e01f50
Phillip Harden <phillipharden@Phillips-MacBook-Pro.local>
2023-03-04 23:02:25 -0500

    error handling updates

--- grader.rb
+++ grader.rb
@@ -1,36 +1,48 @@
 class Grader
-    def initialize
+  def initialize
+    loop do
       print "What is your name? "
       @name = gets.chomp
-      print "What is the name of your assignment? "
-      @assignment = gets.chomp
-      print "What is the number grade of your assignment? "
-      @grade = Float(gets.chomp)
+      break unless @name.empty?
+      puts "Name cannot be blank"
     end
 
-    raise ArgumentError.new("Grade should be between 0 and 100") unless @grade.between?(0, 100)
-  end
-    
-    def convert_grade
-      if @grade >= 90
-        "A"
-      elsif @grade >= 80
-        "B"
-      elsif @grade >= 70
-        "C"
-      elsif @grade >= 60
-        "D"
-      else
-        "F"
-      end
+    loop do
+      print "What is the name of your assignment? "
+      @assignment = gets.chomp
+      break unless @assignment.empty?
+      puts "Assignment name cannot be blank"
+    end
+
+    loop do
+      print "What is the number grade of your assignment? "
+      @grade = Float(gets.chomp)
+      break if @grade.between?(0, 100)
+      puts "Grade should be between 0 and 100"
+    end
+  endnode grader.js
+  
+
+  def convert_grade
+    if @grade >= 90
+      "A"
+    elsif @grade >= 80
+      "B"
+    elsif @grade >= 70
+      "C"
+    elsif @grade >= 60
+      "D"
+    else
+      "F"
     end
   end
-  
-  begin
-    grader = Grader.new
-    letter_grade = grader.convert_grade
-    puts "#{grader.instance_variable_get(:@name)} got a #{letter_grade} on #{grader.instance_variable_get(:@assignment)}"
-  rescue Exception => e
-    puts "Error Message: #{e}"
-  end
-  
+end
+
+begin
+  grader = Grader.new
+  letter_grade = grader.convert_grade
+  puts "#{grader.instance_variable_get(:@name)} got a #{letter_grade} on #{grader.instance_variable_get(:@assignment)}"
+rescue ArgumentError => e
+  puts "Error Message: #{e}"
+  retry
+end
